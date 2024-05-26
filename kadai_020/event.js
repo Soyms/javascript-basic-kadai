e5802fa28d16094bb29d774ccb71eea6119f15a4
Soyms <166920540+Soyms@users.noreply.github.com>
2024-05-26 18:28:11 +0900

    kadai_020をアップロード

---
+++ kadai_020/event.js
@@ -0,0 +1,15 @@
+const btn = document.getElementById('btn');
+const parentList = document.getElementById('text');
+
+btn.addEventListener('click', () => {
+    
+  // li要素を新しく作成する
+  const childList = document.createElement('text');
+  childList.textContent = 'ボタンをクリックしました';
+
+  // parentListの最初の子要素を削除する
+  parentList.removeChild(parentList.firstChild);
+
+  // childListを追加する
+  parentList.appendChild(childList);
+});
